278d018616bfbd97b8b14d8f10c5e4ae94bfe51a
david clayton <sdsuperkid@gmail.com>
2022-11-21 15:48:29 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var timeDiv = document.getElementById("timerDiv");
+var clicksDiv = document.getElementById("cpsDiv");
+var clicksDiv = document.getElementById("scoreDiv");
+
+
+var gameOver;
+
+var cps;
+
+var time;
+
+var score;
+
+var started = false;
+
+function cpsButtonClicked() {
+
+    if(started){
+       score += 1;
+        scoreDiv.innerHTML = score;
+    } else{
+        reset();
+       }
+
+}
+function reset(){
+    //zero out vars
+    time= 0.00;
+    cps = 0;
+    score = 0;
+    //start game
+    started = true;
+    gameOver = false;
+    setInterval(increaseTime,10)
+    //update to show val
+    timeDiv.innerHTML = time;
+    cpsDiv.innerHTML = cps;
+    scoreDiv.innerHTML = score;
+    
+    
+
+}
+
+
+function updateCPS() {
+    cps = score/time;
+    cpsDiv.innerHTML = cps.toFixed(2);
+
+}
+
+
+function increaseTime() {
+    if(!gameOver){
+        time += 0.01;
+        timeDiv.innerHTML = time.toFixed(1);
+        updateCPS();
+    }
+
+}
+
+window.addEventListener("keydown", (e) => {
+    if(e.key == " "){
+        if (!gameOver) {
+            gameOver = true;
+        } else {
+            reset();
+        }
+    }
+});
