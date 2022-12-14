3155c2d745f10c6563c4dca8a51c99642b77f6dd
david clayton <sdsuperkid@gmail.com>
2022-12-14 14:55:35 -0800

    bug died

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 var timeDiv = document.getElementById("timerDiv");
 var clicksDiv = document.getElementById("cpsDiv");
 var clicksDiv = document.getElementById("scoreDiv");
+var buttonTxt = document.getElementById("buttonTxt");
+var cpsButton = document.getElementById("cpsButton");
 setInterval(increaseTime,10)
 
-var gameOver;
+
+var gameOver = true;
 
 var cps;
 
-var time;
+var time = 0.00;
 
-var score;
+var score = 0;
 
 var started = false;
 
 function cpsButtonClicked() {
 
     if(started){
-       score += 1;
+        buttonTxt.innerHTML="";
+        cpsButton.style.backgroundColor = "#4caf88";
+        setTimeout(resetC, 100);
+        if(gameOver == false){
+            score += 1;
+        }
         scoreDiv.innerHTML = score;
     } else{
         reset();
        }
 
 }
+function resetC(){
+    cpsButton.style.backgroundColor = "#4caf50";
+
+}
 function reset(){
     //zero out vars
     time= 0.00;
-    cps = 0;
-    score = 0;
+    cps = 0.00;
+    score = 0.00;
     //start game
     started = true;
     gameOver = false;
@@ -54,7 +66,11 @@ function increaseTime() {
         time += 0.01;
         timeDiv.innerHTML = time.toFixed(1);
         updateCPS();
+    }else{
+        time = 0.00
+
     }
+    
 
 }
 
