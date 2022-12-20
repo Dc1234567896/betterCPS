cda27e58541ed8b05b78169ba5b384a684e3e02a
david clayton <sdsuperkid@gmail.com>
2022-12-19 16:07:00 -0800

    add password

---
+++ login.js
@@ -0,0 +1,20 @@
+var  user= document.getElementById("user");
+var  password= document.getElementById("password");
+var  enter= document.getElementById("enter");
+// "user": "password",
+var dict = {
+    "hans": "password",
+    "dyl123": "password",
+    "guest": "1234"
+}
+
+function login(){
+
+    if (user.value in dict){
+        if(dict[user.value] == password.value){
+            window.location.replace("http://bettercps.com");
+        }
+    }
+
+
+} 
