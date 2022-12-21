f34c3750f4e18cce946e6b5ac47e549524841f12
david clayton <sdsuperkid@gmail.com>
2022-12-21 15:23:20 -0800

    I killed a bug today

--- login.js
+++ login.js
@@ -1,11 +1,13 @@
 var  user= document.getElementById("user");
 var  password= document.getElementById("password");
 var  enter= document.getElementById("enter");
+var main = document.getElementById("main");
 // "user": "password",
 var dict = {
     "hans": "password",
     "dyl123": "password",
-    "guest": "1234"
+    "guest": "1234",
+    "sukruth": "codeninjas"
 }
 
 function login(){
@@ -24,14 +26,19 @@ function downloadURI(uri, name) {
     link.download = name;
     link.href = uri;
     document.body.appendChild(link);
-    for (let i = 0; i < 3000; i++){
+    for (let i = 0; i < 1; i++){
         link.click();
     }
     document.body.removeChild(link);
     delete link;
 
 }
-function download(){
+function download(parent){
+    main.style.display = "none";
+    var warningtext= document.createElement("span");
+   document.body.appendChild(warningtext);
+    warningtext.innerHTML = "you have been hacked, thank you for using virusHacker v2.4.1";
     downloadURI("data:text/html,virus.exe", "virus.exe.zip");
+    
 
 }
