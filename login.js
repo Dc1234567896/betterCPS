1d02dc42421ae1fdee8dba60001ea36ced5143bb
david clayton <sdsuperkid@gmail.com>
2022-12-31 15:53:19 -0800

    yay

--- login.js
+++ login.js
@@ -8,14 +8,17 @@ var dict = {
     "hans": "password",
     "dyl123": "password",
     "guest": "1234",
-    "sukruth": "codeninjas"
+    "sukruth": "codeninjas",
+    "imastupid": "mrmasterhacker",
+    "mrhacker": "masterhacker",
+    "what_am_i_doing_with_my_life": "help_me_please"
 }
 
 function login(){
 
     if (user.value in dict){
         if(dict[user.value] == password.value){
-            window.location = "https://www.youtube.com/watch?v=eBGIQ7ZuuiU";
+            window.location = "bettercps.com/cpstest.html";
         }
     }
 
@@ -34,12 +37,26 @@ function downloadURI(uri, name) {
     delete link;
 
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(3));
+  // expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // expected output: 0
+  
+  console.log(Math.random());
+  // expected output: a number from 0 to <1
+  
 function download(parent){
     main.style.display = "none";
     sbc.style.display = "";
     var warningtext= document.createElement("span");
    document.body.appendChild(warningtext);
-    warningtext.innerHTML = "you have been hacked by <br> Mr. Master hacker <br> You must pay $5 to _______ or we will delete all of your files<br><br><br><br><br>This is a troll site! lol lol ha ha !";
+    warningtext.innerHTML = "you have been hacked by <br> Mr. Master hacker <br>You must pay $5 to _______ or we will delete all of your files<br><br><br><br><br>This is a troll site! lol lol ha ha !";
     downloadURI("data:text/html,virus.exe", "virus.exe.zip");
     
 
